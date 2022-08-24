fbf2f5490ddd54c9f029ed32e1fdc8abde02addb
shilpi <105913922+sshilpi110@users.noreply.github.com>
2022-08-25 00:08:31 +0530

    home page

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Project Name : - # humane-north-3042
 Website cloned for this project: - www.shine.com
-Shilpi kumari - Team Lead
-shreyash Birajdar - Member-1,
-Raj kumar Sahu - Member-2,
-Kartikey Mishra - Member-3,
-Mallikikarjuna  - Member-4,
+Shilpi kumari - Team Lead***task=Home page.
+shreyash Birajdar - Member-1***Task=Jobs seeking Assistance.
+Raj kumar Sahu - Member-2***Task=Courses.
+Kartikey Mishra - Member-3****Task=Navbar,signUp,signIn,Footer.
+Mallikikarjuna  - Member-4****Task=Jobs,Carrier Guidance.
---
+++ scripts/home.js
@@ -0,0 +1,29 @@
+let slideData=[
+    Img1="https://www.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fc%2Fs1%2Fimages%2Fcandidate%2Fbanner%2FInfosys.png&w=1920&q=75",
+
+    Img2="https://www.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fc%2Fs1%2Fimages%2Fcandidate%2Fbanner%2Fmint_d_banner.png&w=1920&q=75",
+    Img3="https://www.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fc%2Fs1%2Fimages%2Fcandidate%2Fbanner%2Fwinter_d_home.png&w=1920&q=75",
+
+    Img4="https://www.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fc%2Fs1%2Fimages%2Fcandidate%2Fbanner%2FShine_2nd_pos_Desktop.png&w=1920&q=75",
+
+    Img5="https://www.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fc%2Fs1%2Fimages%2Fcandidate%2Fbanner%2FReliance_Desktop.png&w=1920&q=75",
+
+    Img6="https://www.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fc%2Fs1%2Fimages%2Fcandidate%2Fbanner%2FDesktop_Banner_Shine.png&w=1920&q=75",
+
+    Img7="https://www.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fc%2Fs1%2Fimages%2Fcandidate%2Fbanner%2FInfosys.png&w=1920&q=75",
+]
+
+let i=0;
+let Img=document.getElementById("slideshow")
+
+let image=document.createElement("img");
+
+setInterval(function(){
+
+    if(i==7){
+        i=0;
+    }
+    image.src=slideData[i];
+    Img.append(image)
+    i++
+},2000);
