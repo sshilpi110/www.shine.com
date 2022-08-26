217b4fe413d663eb61e324e042d877dadbd1e5b8
Shreyash <birajdarshreyash@gmail.com>
2022-08-27 00:27:51 +0530

    import export completed of navbar

---
+++ jobSeeking.js
@@ -0,0 +1,3 @@
+import navbar from "./navbar.js";
+document.getElementById("navbarShine").innerHTML=navbar();
+
---
+++ navbar.js
@@ -0,0 +1,54 @@
+let navbar = ()=> {
+    return `<div id="navbar">
+    <div id="navbar_1">
+        <div>
+            <a href="https://play.google.com/store/apps/details?id=com.net.shine&referrer=utm_source%3Ddesktop%26utm_medium%3Dorganic"><img style="height:27px;" src="https://upww.screenrec.com/images/f_YH1ZtmyorGCgAnNXQD6kSzxaTbUh5e30.png" alt=""></a>
+        </div>
+        <div>
+            <a href="https://recruiter.shine.com/"><img style="height:28px;" src="https://up129in3.screenrec.com/images/f_h62m0WTqrHiXpOvR8NnzYuCZlIPJKVDA.png" alt=""></a>
+        </div>
+        <div id="Help">
+            <span> <img style="height:27px" src="https://upww.screenrec.com/images/f_GjC7i21goxMuAwzF6HmBbnU9lt4dWEDR.png" alt=""></span>
+            <div id="Help_DD">
+                <h2>080 - 10062222</h2>
+                <p>9am - 6pm, Mon to Sat</p>
+            </div>
+        </div>
+    </div>
+    <div id="navbar_2">
+        <div id="navbar_2_1">
+            <img src="https://www.shine.com/next/static/images/shine-logo.png" alt="">
+            <input id="navbarSearch" type="text" placeholder="Job title, skills">
+            <img src="https://www.transparentpng.com/thumb/search-button/f2qMti-search-button-square-blue-free-png.png" alt="">
+        </div>
+        <div id="navbar_2_2">
+            <img src="https://upww.screenrec.com/images/f_7QvGdIeM1bhUrOHxEFPRfkomNC8DjzlY.png" alt="">
+            <img src="https://upww.screenrec.com/images/f_SqT4blAv7tDGkcWYReFpHUwug5CXoP08.png" alt="">
+            <img src="https://upww.screenrec.com/images/f_vdW94NclGjnYIMqCzuLbw3s0oTpPXytm.png" alt="">
+        </div>
+    </div>
+    <hr>
+    <div id="navbar_3">
+        <div>
+            <p>HOME</p>
+        </div>
+        <div>
+            <p>JOBS</p>
+        </div>
+        <div>
+            <p>JOBS SEEKING ASSISTANCE </p>
+        </div>
+        <div>
+            <p>COURSES</p>
+        </div>
+        <div>
+            <a style="text-decoration: none;color: black;" href="https://learning.shine.com/user-intent"><p>CAREER GUIDANCE</p></a>
+        </div>
+        <div>
+            <a style="text-decoration: none;color: black;" href="https://learning.shine.com/talenteconomy/"><p>BLOG</p></a>
+        </div>
+    </div>
+</div>`
+}
+
+export default navbar;
